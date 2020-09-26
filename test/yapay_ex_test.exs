142a70eb749c5a0cdef716f4ef90849427fc22d3
Paulo da Silva <paulo_silvalearn@hotmail.com>
2020-09-26 11:53:38 -0300

    Move create_order to YapayEx module.

--- test/yapay_ex_test.exs
+++ test/yapay_ex_test.exs
@@ -2,7 +2,37 @@ defmodule YapayExTest do
   use ExUnit.Case
   doctest YapayEx
 
-  test "greets the world" do
-    assert YapayEx.hello() == :world
+  alias Fixtures.{FakeOrder, YapayResponses}
+
+  describe "create_order/1" do
+    setup do
+      bypass = Bypass.open()
+      endpoint = yapay_endpoint(bypass)
+      System.put_env("YAPAY_DOMAIN", endpoint)
+
+      {:ok, bypass: bypass}
+    end
+
+    test "creates an order", %{bypass: bypass} do
+      response = YapayResponses.load("card_transaction_response")
+
+      Bypass.expect(
+        bypass,
+        "POST",
+        "/api/v3/transactions/payment",
+        fn conn ->
+          conn
+          |> Plug.Conn.put_resp_header("content-type", "application/json")
+          |> Plug.Conn.resp(200, response)
+        end
+      )
+
+      order = FakeOrder.credit_card()
+      decoded_response = Jason.decode!(response)
+
+      assert {:ok, 200, ^decoded_response} = YapayEx.create_order(order)
+    end
+
+    defp yapay_endpoint(%{port: port}), do: "http://localhost:#{port}"
   end
 end
